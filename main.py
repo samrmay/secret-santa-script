e0e2a0a97756f8921cab6ea38cc323921b1bf685
Sam May <samrmay@sas.upenn.edu>
2021-01-06 23:46:57 -0500

    added working debug option

--- main.py
+++ main.py
@@ -9,16 +9,18 @@ def parse():
         description='Create a secret santa pool and send out results')
     parser.add_argument('participants_path', metavar='participants', type=str,
                         help='path to a file containing newline delimited list of participants in form "f_name l_name,_email". See example.txt for more info',
-                        default='./participants.txt')
+                        nargs='?',
+                        default='./example.txt')
     parser.add_argument('sender_email', type=str,
                         help='Email address from which secret santas will be sent their recipients',
+                        nargs='?',
                         default='dummy@email.com')
     parser.add_argument('sender_password', type=str,
                         help='Password for sender email',
+                        nargs='?',
                         default='securepassword123')
-    parser.add_argument('-d', '--debug', type=bool,
-                        help='If true, emails will be sent. If false, generated pairs will be printed to console',
-                        default=False)
+    parser.add_argument('-d', '--debug', action='store_true',
+                        help='If true, emails will be sent. If false, generated pairs will be printed to console')
     parser.add_argument('-p', '--port', type=str, default='465')
 
     args = parser.parse_args()
@@ -51,29 +53,34 @@ def get_participants(infile):
     with open(infile) as infile:
         for line in infile:
             arr = line.split(',')
+            if arr[1].endswith('\n'):
+                arr[1] = arr[1][:-1]
             participants.append((arr[0], arr[1]))
     return participants
 
 
-def send_emails(pairs, sender_email, sender_password, debug, port):
+def send_emails(pairs, sender_email, sender_password, port):
     context = ssl.create_default_context()
     with smtplib.SMTP_SSL("smtp.gmail.com", port, context=context) as server:
         server.login(sender_email, sender_password)
-
-        if debug:
-            for email in pairs.keys():
-                print(email, pairs[email])
-        else:
-            for email in pairs.keys():
-                tup = pairs[email]
-                message = f"Subject: Secret Santa \n\nDear {tup[0]}, \n\nYour secret santa recipient is {tup[1]}. Don't tell anyone.\n\n Best regards,\n your mysterious benefactor"
-                server.sendmail(
-                    sender_email, email, message)
+        for email in pairs.keys():
+            tup = pairs[email]
+            message = f"Subject: Secret Santa \n\nDear {tup[0]}, \n\nYour secret santa recipient is {tup[1]}. Don't tell anyone.\n\n Best regards,\n your mysterious benefactor"
+            server.sendmail(
+                sender_email, email, message)
 
 
 def run():
     infile, sender_email, sender_password, debug, port = parse()
     participants = get_participants(infile)
     pairs = create_random_pairs(participants)
-    send_emails(pairs, sender_email, sender_password, debug, port)
-    print('done')
+    if debug:
+        print('=====DEBUG RESULTS=====')
+        print([f'{pairs[x][0]}({x}) got {pairs[x][1]}' for x in pairs.keys()])
+    else:
+        send_emails(pairs, sender_email, sender_password, port)
+        print('done')
+
+
+if __name__ == "__main__":
+    run()
